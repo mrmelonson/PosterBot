13d9b1899650c772db9fe8b11a29ee4c4a37fea7
Zelenyy <dangerdude119@gmail.com>
2020-07-31 23:12:15 +1000

    added ability to upload links to database

--- Zel_Piss_Bot.js
+++ Zel_Piss_Bot.js
@@ -1,63 +1,42 @@
-const { MongoDB } = require('mongodb');
+const { MongoDB, MongoClient } = require('mongodb');
 const { Telegraf } = require('telegraf');
 
+//Database stuff
+var url = "mongodb://localhost:27017/";
+
 //Dont share this key
-const key = require("./key.json");
+const key = require("./.secret/key.json");
 const bot = new Telegraf(key.key);
 
 //try /start in the bot's dms
-bot.start((ctx) => ctx.reply('Welcome!'))
+bot.start((ctx) => ctx.reply('Welcome!'));
 
-//When it detects any text
-bot.on('text', (ctx) => {
-    if (ctx.message.text.toLowerCase().replace(/ /g,'').trim().includes("vore")) {
-        //look for vore here
-        console.log("Vore has been mentioned")
-        if(global.voreTime < 0) {
-            global.voreTime = new Date().getTime();
-            ctx.reply('👀');
-        } else {
-            //TIME TIME TIME
-            var s = new Date().getTime() - global.voreTime;
-            var ms = s % 1000;
-            s = s / 1000;
-            var days = Math.floor(s / 86400);
-            s %= 86400;
-            var hrs = Math.floor(s / 3600);
-            s %= 3600;
-            var mins = Math.floor(s / 60);
-            var secs = Math.floor(s % 60);
-
-            //Formatting nicely
-            var time = ""
-            if(days > 0) {
-                time += `${days} days `;
-            } else if (hrs > 0) {
-                time += `${hrs} hours `;
-            } else if (mins > 0) {
-                time += `${mins} minutes and `;
-            } 
-            time += `${secs}.${pad(ms,3)} seconds`;
-
-            //Print print print
-            ctx.reply(`it has been ${time} since someone has mentioned vore`);
-            global.voreTime = new Date().getTime();
-        }
+bot.command('upload', (ctx) => {
+    if (ctx.from.id != "399871144") {
+        ctx.reply("I'm sorry John");
+        return;
     }
-});
 
+    var link = ctx.message.text.split(' ')[1];
+
+    var client = new MongoClient(url);
+
+    client.connect((err, db) => {
+        if (err) throw err;
+        var dbo = db.db("Telegram_Test_DB");
+        var pisspic = { url: link };
+        dbo.collection("Piss").insertOne(pisspic);
+        db.close();
+    });
+    
+    ctx.reply("Received!")
+
+});
 
 //Start the bot with a little message
 bot.launch().then((o, e) => {
     console.log("Starting bot...");
-    global.voreTime = -1;
 });
-
-//Pad function for time element in the voretime
-function pad(n, z) {
-    z = z || 2;
-    return ('00' + n).slice(-z);
-}
 
 
 
